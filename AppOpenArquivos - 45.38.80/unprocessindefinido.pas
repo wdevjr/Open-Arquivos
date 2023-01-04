a7d4124a09e1be12d07320122db75695a6a7dcff
Walter Ribeiro Homem Junior <10689247+wdevjr@users.noreply.github.com>
2023-01-04 16:01:12 -0400

    Correção da Consulta especial da camada Arquivo- Novo - 2023

---
+++ AppOpenArquivos - 45.38.80/unprocessindefinido.pas
@@ -0,0 +1,30 @@
+unit UnProcessIndefinido;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls;
+
+type
+
+  { TfrmSplash }
+
+  TfrmSplash = class(TForm)
+    Label1: TLabel;
+    ProgressBar1: TProgressBar;
+  private
+
+  public
+
+  end;
+
+var
+  frmSplash: TfrmSplash;
+
+implementation
+
+{$R *.lfm}
+
+end.
